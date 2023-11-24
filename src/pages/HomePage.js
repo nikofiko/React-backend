6e394b28733b7a0acc0dd6413e9842d26ef97429
Adrian Flak <nikodemwilk69@gmail.com>
2023-11-24 15:43:15 +0100

    second commit 2

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import WithSubnavigation from '../components/Navigation'
 import httpClient from '../utils/httpClient'
+import { useNavigate } from 'react-router-dom'
 
-export default function HomePage() {
+function HomePage() {
   const [travels, setTravels] = useState([])
   const [weather, setWeather] = useState([])
-  const [user, setUser] = useState([null])
+  const [user, setUser] = useState(null)
 
-  useEffect(() =>{
-    getData()
+  const navigation = useNavigate()
+
+  useEffect(()=>{
+      getData()
   },[])
 
   const getData = async () => {
@@ -16,10 +19,19 @@ export default function HomePage() {
     console.log(data)
   }
 
+  const handleLogout = async () => {
+    const {data} = await httpClient.get("/logout")
+    navigation("/login")
+  }
+
   return (
     <div>
         <WithSubnavigation/>
         <h1>Strona główna</h1>
+
+        <button onClick={()=>handleLogout()}>Wyloguj</button>
     </div>
   )
 }
+
+export default HomePage 
