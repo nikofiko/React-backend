722eeeafb60cdd6adaa43fcb85ac208e9a60eda5
Adrian Flak <nikodemwilk69@gmail.com>
2023-11-24 15:24:22 +0100

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import WithSubnavigation from '../components/Navigation'
+import httpClient from '../utils/httpClient'
+
+export default function HomePage() {
+  const [travels, setTravels] = useState([])
+  const [weather, setWeather] = useState([])
+  const [user, setUser] = useState([null])
+
+  useEffect(() =>{
+    getData()
+  },[])
+
+  const getData = async () => {
+    const {data} = await httpClient.get("http://127.0.0.1:5000/dashboard")
+    console.log(data)
+  }
+
+  return (
+    <div>
+        <WithSubnavigation/>
+        <h1>Strona główna</h1>
+    </div>
+  )
+}
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,82 @@
+ // src/components/LoginPage.js
+ import React, { useState } from 'react';
+ import {
+   Box,
+   FormControl,
+   FormLabel,
+   Input,
+   Button,
+   Heading,
+   VStack,
+   useToast,
+ } from '@chakra-ui/react';
+
+ import WithSubnavigation from '../components/Navigation'
+import { useNavigate} from 'react-router-dom';
+import httpClient from '../utils/httpClient';
+ 
+ const LoginPage = () => {
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const toast = useToast()
+   const navigation = useNavigate()
+ 
+   const handleLogin = async () => {
+     // Perform login logic here
+     const {data} = await httpClient.post("http://127.0.0.1:5000/login",{email,password})
+     console.log(data);
+
+     toast({
+        title: data['message'],
+        status: data["ok"] ? "success" : "error",
+        duration: 4000,
+        isClosable: true,
+    })
+
+    if (data["ok"]) {
+      navigation('/')
+    }
+
+   };
+ 
+   return (
+
+    <div>
+    <WithSubnavigation/>
+    <VStack marginTop={20} spacing={8} align="center">
+      <Box p={8} borderWidth={1} borderRadius={8} boxShadow="lg" width="400px">
+        <Heading mb={4}>Login</Heading>
+        <FormControl>
+          <FormLabel>E-mail</FormLabel>
+          <Input
+            type="text"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormControl>
+        <FormControl mt={4}>
+          <FormLabel>Password</FormLabel>
+          <Input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </FormControl>
+        <Button
+          colorScheme="teal"
+          mt={4}
+          onClick={handleLogin}
+          isFullWidth
+        >
+          Login
+        </Button>
+      </Box>
+    </VStack>
+    </div>
+   );
+ };
+ 
+ export default LoginPage;
+  
